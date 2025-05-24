2955b3c728c59303dbdd7a49b233101f55222a40
letanure <letanure@gmail.com>
2025-05-24 21:49:04 +0200

    feat: add request user input

--- src/local-model.ts
+++ src/local-model.ts
@@ -1,31 +1,13 @@
-// name: Local model with LM studio
+// name: Local model with LM Studio
 
+import 'dotenv/config'
+import readline from 'node:readline/promises'
+import { stdin as input, stdout as output } from 'node:process'
 import { createOpenAICompatible } from '@ai-sdk/openai-compatible'
 import { generateText } from 'ai'
 
-import type { CoreTool } from 'ai'
-import type { z } from 'zod'
-
-/**
- * Matt here!
- *
- * I have a weird WSL setup which means I have occasional
- * trouble connecting to localhost. So, this is a me-only
- * workaround.
- */
 const getLocalhost = () => {
   return process.env.LOCALHOST_OVERRIDE || 'localhost'
-}
-
-type GetZodObjectFromCoreTool<T> =
-  T extends CoreTool<infer Z extends z.ZodType> ? z.infer<Z> : never
-
-type GetToolExecutionMapFromTools<
-  TTools extends Record<string, CoreTool<any>>,
-> = {
-  [K in keyof TTools]?: (
-    args: GetZodObjectFromCoreTool<TTools[K]>
-  ) => Promise<any>
 }
 
 const lmstudio = createOpenAICompatible({
@@ -45,8 +27,12 @@ export const askLocalLLMQuestion = async (input: string) => {
   return text
 }
 
-const input = `Tell me a story about your grandmother.`
+// Prompt user for input
+const rl = readline.createInterface({ input, output })
 
-const localLLMResult = await askLocalLLMQuestion(input)
+const userInput = await rl.question('🧠 Ask your local LLM: ')
+rl.close()
 
-console.log(localLLMResult)
+const localLLMResult = await askLocalLLMQuestion(userInput)
+
+console.log('\n🤖', localLLMResult)
