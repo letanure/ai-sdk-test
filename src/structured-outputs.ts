0d5a3d17408e5155168c3c46011e74a7f0cf92a0
letanure <letanure@gmail.com>
2025-05-25 08:00:25 +0200

    feat: add example with structured output

---
+++ src/structured-outputs.ts
@@ -0,0 +1,42 @@
+// name: Structured outputs
+
+import 'dotenv/config'
+import { generateObject } from 'ai'
+import { z } from 'zod'
+import { openai } from '@ai-sdk/openai'
+
+const model = openai('gpt-4o-mini-2024-07-18')
+
+const schema = z.object({
+  recipe: z.object({
+    name: z.string().describe('The title of the recipe'),
+    ingredients: z
+      .array(
+        z.object({
+          name: z.string(),
+          amount: z.string(),
+        })
+      )
+      .describe('The ingredients needed for the recipe'),
+    steps: z.array(z.string()).describe('The steps to make the recipe'),
+  }),
+})
+
+export const createRecipe = async (prompt: string) => {
+  const { object } = await generateObject({
+    model,
+    schema,
+    prompt,
+    schemaName: 'Recipe',
+    system:
+      `You are helping a user create a recipe. ` +
+      `Use British English variants of ingredient names,` +
+      `like Coriander over Cilantro.`,
+  })
+
+  return object.recipe
+}
+
+const recipe = await createRecipe('How to make baba ganoush?')
+
+console.dir(recipe, { depth: null })
